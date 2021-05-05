318b681412a2a2201d19907f86a72dbfefabebb0
Eun Jung Choi <im.your.energy@gmail.com>
2021-05-06 00:33:37 +0900

    Add on_message testcase (async test)

--- tests/test_dc_butler.py
+++ tests/test_dc_butler.py
@@ -1,4 +1,21 @@
-from dc_butler.init__ import split_message_argument, convert_time_to_seconds
+import pytest
+from dc_butler.__init__ import split_message_argument, convert_time_to_seconds
+from dc_butler import on_message
+
+
+class MockMessage:
+    def __init__(self, content):
+        self.content = content
+        self.author = 1
+        self.channel = MockChannel()
+
+
+class MockChannel:
+    def __init__(self):
+        self.send_buffer = []
+
+    async def send(self, content):
+        self.send_buffer.append(content)
 
 
 def test_split_message_argument():
@@ -8,3 +25,11 @@ def test_split_message_argument():
 def test_convert_time_to_seconds():
     assert convert_time_to_seconds('1분 30초') == 90
     assert convert_time_to_seconds('10분 ') == 600
+
+
+@pytest.mark.asyncio
+async def test_on_message():
+    message = MockMessage('/타이머 5초')
+    await on_message(message)
+    assert message.channel.send_buffer[0] == '5초 뒤에 알려드릴게요 :ok_hand:'
+    assert message.channel.send_buffer[1] == '5초 됐습니다 :coffee:'
