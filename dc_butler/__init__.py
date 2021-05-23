044b9ad65d51529a5a0d548dabc757c8458c9803
Eun Jung Choi <im.your.energy@gmail.com>
2021-05-23 17:51:56 +0900

    Add separate local bot env from production env

--- dc_butler/__init__.py
+++ dc_butler/__init__.py
@@ -1,18 +1,25 @@
 import asyncio
 import os
+import sys
 
 import discord
 
 client = discord.Client()
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKENS = {
+    'local': 'DC_TOKEN_LOCAL',
+    'test': 'DC_TOKEN_TEST',
+    'production': 'DISCORD_TOKEN'
+}
+ENV = 'local'
 help_message = """원하는 시간을 입력해주세요. 그때가 되면 타이머봇이 시간을 알려드릴게요.
 예시) 10분, 1시간 25분, 2분 30초
 """
+LOCAL_BOT_CHANNEL_ID = os.getenv("LOCAL_BOT_CHANNEL_ID")
 
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {ENV} server: {client.user}')
 
 
 async def timer_handler(message, arg):
@@ -30,6 +37,10 @@ async def timer_handler(message, arg):
 @client.event
 async def on_message(message):
     if message.author == client.user:
+        return
+    if message.channel.id == int(LOCAL_BOT_CHANNEL_ID) and ENV != 'local':
+        return
+    if message.channel.id != int(LOCAL_BOT_CHANNEL_ID) and ENV == 'local':
         return
 
     dispatch_handlers = {
@@ -68,4 +79,6 @@ def convert_time_to_seconds(arg):
 
 
 if __name__ == '__main__':
+    ENV = sys.argv[1]
+    TOKEN = os.getenv(TOKENS[ENV])
     client.run(TOKEN)
