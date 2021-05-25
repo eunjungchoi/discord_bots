e8ce962a4ef93acb5889631b5f2fad64f7ce0a3c
Eun Jung Choi <im.your.energy@gmail.com>
2021-05-25 21:41:53 +0900

    Refactor rename client to bot

--- dc_butler/client.py
+++ dc_butler/client.py
@@ -6,16 +6,16 @@ import discord
 
 ENV = 'local'
 
-client = discord.Client()
+bot = discord.Client()
 help_message = """원하는 시간을 입력해주세요. 그때가 되면 타이머봇이 시간을 알려드릴게요.
 예시) 10분, 1시간 25분, 2분 30초
 """
 LOCAL_BOT_CHANNEL_ID = os.getenv("LOCAL_BOT_CHANNEL_ID", '')
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {ENV} server: {client.user}')
+    print(f'We have logged in as {ENV} server: {bot.user}')
 
 
 async def timer_handler(message, arg):
@@ -44,9 +44,9 @@ def message_allowed(channel_id):
     return False
 
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     if not message_allowed(message.channel.id):
         return
@@ -91,4 +91,4 @@ def convert_time_to_seconds(arg):
 
 
 def run(token):
-    client.run(token)
+    bot.run(token)
